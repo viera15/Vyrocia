34949f0f6811d44541a80637583693e0bbe5f1c8
Viera Rajnáková <vierarajnakova@gmail.com>
2024-04-23 22:35:38 +0200

    doplnenie popisov tabuliek

---
+++ sql/popisy_table.sql
@@ -0,0 +1,20 @@
+COMMENT ON TABLE public.osobnost
+  IS 'Základná tabuľka pre informácie o osobnosti';
+
+COMMENT ON TABLE public.pohlavie
+  IS 'Číselník - pohlavia';
+
+COMMENT ON TABLE public.povolania_osobnosti
+  IS 'Spojovacia tabuľka osobnost - povolanie (žena - muž). Možnosť zapísania viac povolaní pre jednu osobnosť. ';
+
+COMMENT ON TABLE public.povolanie_muz
+  IS 'Číselník - povolania muži';
+
+COMMENT ON TABLE public.povolanie_zena
+  IS 'Číselník - povolania ženy';
+
+COMMENT ON TABLE public.privlastok_muz
+  IS 'Číselník - prívlastok muž';
+
+COMMENT ON TABLE public.privlastok_zena
+  IS 'Číselník - prívlastok žena';
