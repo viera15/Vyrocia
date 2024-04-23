368f7c95d37ba59da53f4d433109155940543ee6
Viera Rajnáková <vierarajnakova@gmail.com>
2024-04-23 22:34:47 +0200

    naplnenie tabuľky prívlastok žena

---
+++ sql/insert_into_table_privlastok_zena.sql
@@ -0,0 +1,7 @@
+INSERT INTO privlastok_zena(privlastok_z, datum_vytvorenia)
+VALUES ('neurčená',CURRENT_TIMESTAMP),
+       ('slovenská',CURRENT_TIMESTAMP),
+	   ('švédska',CURRENT_TIMESTAMP),
+	   ('škótska',CURRENT_TIMESTAMP),
+	   ('americká',CURRENT_TIMESTAMP),
+	   ('nemecká',CURRENT_TIMESTAMP);
