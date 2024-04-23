6e3d739a58e73fe96a4e807b6b773981ee9d3f18
Viera Rajnáková <vierarajnakova@gmail.com>
2024-04-23 22:32:27 +0200

    naplnenie tabuľky povolania žena

---
+++ sql/insert_into_table_povolanie_zena.sql
@@ -0,0 +1,9 @@
+INSERT INTO povolanie_zena(povolanie_z, datum_vytvorenia)
+VALUES ('neurčená',CURRENT_TIMESTAMP),
+       ('huslistka',CURRENT_TIMESTAMP),
+	   ('speváčka (soprán)',CURRENT_TIMESTAMP),
+	   ('operná speváčka (soprán)',CURRENT_TIMESTAMP),
+	   ('muzikologička',CURRENT_TIMESTAMP),
+	   ('huslistka',CURRENT_TIMESTAMP),
+	   ('operná speváčka (mezzosoprán)',CURRENT_TIMESTAMP),
+	   ('pedagogička',CURRENT_TIMESTAMP);
